bacc31f06dc9133c532f2fdfe36064c970296aad
kamkanev <kamkanev@gmail.com>
2025-12-22 17:26:09 +0100

    added node and edges rng generation in rrt

--- rrt/RRT.py
+++ rrt/RRT.py
@@ -9,6 +9,8 @@ def main():
     obstacles_dimensions = 50
     obstacles_number = 30
 
+    iteration = 0
+
     pygame.init()
     map = RRTMap(start, goal, dimensions, obstacles_dimensions, obstacles_number)
     graph = RRTGraph(start, goal, dimensions, obstacles_dimensions, obstacles_number)
@@ -17,9 +19,33 @@ def main():
 
     map.drawMap(obstacles)
 
-    pygame.display.update()
-    pygame.event.clear()
-    pygame.event.wait(0)
+    while(True):
+        x,y = graph.sample_env()
+        n = graph.numberOfNodes()
+        graph.addNode(n, x, y)
+        graph.addEdge(n-1, n)
+        x1,y1 = graph.x[n], graph.y[n]
+        x2,y2 = graph.x[n-1], graph.y[n-1]
+        if graph.isFree():
+            pygame.draw.circle(map.map, map.Red, (graph.x[n], graph.y[n]), map.nodeRad, map.nodeThickness)
+
+            if not graph.crossObstacle(x1, y1, x2, y2):
+                pygame.draw.line(map.map, map.Blue, (x1, y1), (x2, y2), map.edgeThickness)
+        
+        
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                    pygame.quit()
+                    return
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    return
+    
+    # pygame.event.clear()
+    # pygame.event.wait(0)
 
 if __name__ == "__main__":
     main()
--- rrt/RRTbase.py
+++ rrt/RRTbase.py
@@ -14,7 +14,7 @@ class RRTMap:
         self.map = pygame.display.set_mode((self.mapw, self.maph))
         self.map.fill((255, 255, 255))
 
-        self.nodeRad = 0
+        self.nodeRad = 5
         self.nodeThickness = 0
         self.edgeThickness = 1
 
@@ -74,12 +74,12 @@ class RRTGraph:
         self.path = []
 
     def addNode(self, n, x, y):
-        self.x.append(n, x)
-        self.y.append(n, y)
+        self.x.insert(n, x)
+        self.y.append(y)
         
 
     def addEdge(self, parent, child):
-        self.parent.append(child, parent)
+        self.parent.insert(child, parent)
 
     def makeRandomRect(self):
         x = int(random.uniform(0, self.mapw - self.obsticles_dimensions))
@@ -127,14 +127,39 @@ class RRTGraph:
         pass
 
     def isFree(self):
-        pass
+        n = self.numberOfNodes() - 1
+        (x,y) = (self.x[n], self.y[n])
+        obs = self.obsticles.copy()
+        while len(obs) > 0:
+            rect = obs.pop()
+            if rect.collidepoint(x, y):
+                self.removeNode(n)
+                return False
+        return True
         
 
-    def crossObstacle(self):
-        pass
+    def crossObstacle(self, x1, y1, x2, y2):
+        obs = self.obsticles.copy()
+        while len(obs) > 0:
+            rect = obs.pop()
+            for i in range(0,101):
+                u = i / 100
+                x = x1 * u + x2 * (1 - u)
+                y = y1 * u + y2 * (1 - u)
+                if rect.collidepoint(x, y):
+                    return True
+        return False
+        
 
-    def connect(self):
-        pass
+    def connect(self, n1, n2):
+        (x1, y1) = (self.x[n1], self.y[n1])
+        (x2, y2) = (self.x[n2], self.y[n2])
+        if self.crossObstacle(x1, y1, x2, y2):
+            self.removeNode(n2)
+            return False
+        else:
+            self.addEdge(n1, n2)
+            return True
 
     def step(self):
         pass
