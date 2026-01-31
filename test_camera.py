9cf75f01487b54a98298249ffdb6624f50816ec0
kamkanev <kamkanev@gmail.com>
2026-01-31 13:33:50 +0100

    small fixes to camera

--- test_camera.py
+++ test_camera.py
@@ -22,7 +22,18 @@ async def main():
                 arr = np.frombuffer(base64.b64decode(data), np.uint8)
                 img = cv2.imdecode(arr, cv2.IMREAD_COLOR)
                 if img is not None:
+                    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+                    lower_red = np.array([0, 100, 100])
+                    upper_red = np.array([10, 255, 255])
+                    mask1 = cv2.inRange(hsv, lower_red, upper_red)
+
+                    result = cv2.bitwise_and(img, img, mask=mask1)
+
+                    cv2.rectangle(img,(384,0),(510,128),(0,255,0),3)
+
                     cv2.imshow("frame", img)
+                    cv2.imshow("mask", mask1)
+                    cv2.imshow("result", result)
                     if cv2.waitKey(1) & 0xFF == 27:
                         break
 
